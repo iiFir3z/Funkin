4ebecea708c941133d4f4cbae53e1b8a689b7112
Fire <iifir3z@protonmail.com>
2021-02-04 13:12:58 -0800

    nvm im dumb as fuck

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -235,7 +235,6 @@ class TitleState extends MusicBeatState
 		}
 
 		// this is for backups var pressedEnter:Bool = FlxG.keys.justPressed.ENTER;
-		bind(AButton:String = "ENTER");
 
 		var pressedEnter:Bool = FlxG.keys.justPressed.ENTER;
 
